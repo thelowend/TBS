06afddabd813a19c71a2e070613b1f16844f37f3
thelowend <thelowend6@gmail.com>
2017-07-25 11:43:44 -0300

    modify skills page ongoing

--- App/TBS/src/app/components/manage-skills/manage-skills.component.ts
+++ App/TBS/src/app/components/manage-skills/manage-skills.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../../_models/index';
+import { UserService } from '../../_services/index';
+
 @Component({
   moduleId: module.id,
   selector: 'app-manage-skills',
-  templateUrl: './manage-skills.component.html'
+  templateUrl: './manage-skills.component.html',
+  styleUrls: ['./manage-skills.component.scss']
 })
 export class ManageSkillsComponent implements OnInit {
+  currentUser: User;
 
-  constructor() { }
+  constructor(private userService: UserService) {
+      this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+      console.log(this.currentUser);
+  }
 
   ngOnInit() {
   }
